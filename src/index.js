e979b62167b52ea3d1b93f19d710a5aa8243387f
punitsoni22 <punitsoni14476@gmail.com>
2026-04-20 22:48:55 +0530

    fix: update start script in package.json and improve PORT configuration in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 if (NODE_ENV === 'development') dotenv.config();
 
 const config = require('config');
-const PORT = config.get('PORT');
+const PORT = process.env.PORT || config.get('PORT') || 3000;
 
 const app = require('./app');
 app.listen(PORT, () => console.log(`Server is running in ${NODE_ENV} mode on port: ${PORT}`));
